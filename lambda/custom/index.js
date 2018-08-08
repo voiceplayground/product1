56326a7e35073685a3b3b0ca317ca8d34e6646c5
Luke Little <me@lukelittle.com>
2018-08-08 04:28:55 -0700

    various. sorry for pushing to master

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,240 +1,258 @@
 /* eslint-disable  func-names */
+/* eslint-disable  no-restricted-syntax */
+/* eslint-disable  no-loop-func */
+/* eslint-disable  consistent-return */
 /* eslint-disable  no-console */
 
 const Alexa = require('ask-sdk-core');
 
 const LaunchRequestHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-  },
-  handle(handlerInput) {
-    const speechText = 'Hello. I calculate retirement savings gaps!';
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speechText = 'Let\'s calculate your retirement readiness.';
 
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt(speechText)
-      .getResponse();
-  },
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .getResponse();
+    },
 };
 
 const InProgressIntent = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
 
-    return request.type === 'IntentRequest'
-      && request.intent.name === 'RetirementReadinessIntent'
-      && request.dialogState !== 'COMPLETED';
-  },
-  handle(handlerInput) {
-    const currentIntent = handlerInput.requestEnvelope.request.intent;
-    // let prompt = '';
+        return request.type === 'IntentRequest'
+            && request.intent.name === 'RetirementReadinessIntent'
+            && request.dialogState !== 'COMPLETED';
+    },
+    handle(handlerInput) {
+        const currentIntent = handlerInput.requestEnvelope.request.intent;
+        let prompt = '';
 
-    // for (const slotName of Object.keys(handlerInput.requestEnvelope.request.intent.slots)) {
-    //   const currentSlot = currentIntent.slots[slotName];
-    //   if (currentSlot.confirmationStatus !== 'CONFIRMED'
-    //             && currentSlot.resolutions
-    //             && currentSlot.resolutions.resolutionsPerAuthority[0]) {
-    //     if (currentSlot.resolutions.resolutionsPerAuthority[0].status.code === 'ER_SUCCESS_MATCH') {
-    //       if (currentSlot.resolutions.resolutionsPerAuthority[0].values.length > 1) {
-    //         prompt = 'Which would you like';
-    //         const size = currentSlot.resolutions.resolutionsPerAuthority[0].values.length;
+        for (const slotName of Object.keys(handlerInput.requestEnvelope.request.intent.slots)) {
+            const currentSlot = currentIntent.slots[slotName];
+            if (currentSlot.confirmationStatus !== 'CONFIRMED'
+                && currentSlot.resolutions
+                && currentSlot.resolutions.resolutionsPerAuthority[0]) {
+                if (currentSlot.resolutions.resolutionsPerAuthority[0].status.code === 'ER_SUCCESS_MATCH') {
+                    if (currentSlot.resolutions.resolutionsPerAuthority[0].values.length > 1) {
+                        prompt = 'Which would you like';
+                        const size = currentSlot.resolutions.resolutionsPerAuthority[0].values.length;
 
-    //         currentSlot.resolutions.resolutionsPerAuthority[0].values
-    //           .forEach((element, index) => {
-    //             prompt += ` ${(index === size - 1) ? ' or' : ' '} ${element.value.name}`;
-    //           });
+                        currentSlot.resolutions.resolutionsPerAuthority[0].values
+                            .forEach((element, index) => {
+                                prompt += ` ${(index === size - 1) ? ' or' : ' '} ${element.value.name}`;
+                            });
 
-    //         prompt += '?';
+                        prompt += '?';
 
-    //         return handlerInput.responseBuilder
-    //           .speak(prompt)
-    //           .reprompt(prompt)
-    //           .addElicitSlotDirective(currentSlot.name)
-    //           .getResponse();
-    //       }
-    //     } else if (currentSlot.resolutions.resolutionsPerAuthority[0].status.code === 'ER_SUCCESS_NO_MATCH') {
-    //       if (requiredSlots.indexOf(currentSlot.name) > -1) {
-    //         prompt = `What ${currentSlot.name} are you looking for`;
+                        return handlerInput.responseBuilder
+                            .speak(prompt)
+                            .reprompt(prompt)
+                            .addElicitSlotDirective(currentSlot.name)
+                            .getResponse();
+                    }
+                } else if (currentSlot.resolutions.resolutionsPerAuthority[0].status.code === 'ER_SUCCESS_NO_MATCH') {
+                    if (requiredSlots.indexOf(currentSlot.name) > -1) {
+                        prompt = `What ${currentSlot.name} are you looking for`;
 
-    //         return handlerInput.responseBuilder
-    //           .speak(prompt)
-    //           .reprompt(prompt)
-    //           .addElicitSlotDirective(currentSlot.name)
-    //           .getResponse();
-    //       }
-    //     }
-    //   }
-    // }
+                        return handlerInput.responseBuilder
+                            .speak(prompt)
+                            .reprompt(prompt)
+                            .addElicitSlotDirective(currentSlot.name)
+                            .getResponse();
+                    }
+                }
+            }
+        }
 
-    // return handlerInput.responseBuilder
-    //   .addDelegateDirective(currentIntent)
-    //   .getResponse();
-
-    return handlerInput.responseBuilder
-      .speak(prompt)
-      .reprompt(prompt)
-      .addElicitSlotDirective(currentSlot.name)
-      .getResponse();
-  },
+        return handlerInput.responseBuilder
+            .addDelegateDirective(currentIntent)
+            .getResponse();
+    },
 };
 
 const CompletedIntent = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
 
-    return request.type === 'IntentRequest'
-      && request.intent.name === 'RetirementReadinessIntent'
-      && request.dialogState === 'COMPLETED';
-  },
-  handle(handlerInput) {
-    const filledSlots = handlerInput.requestEnvelope.request.intent.slots;
+        return request.type === 'IntentRequest'
+            && request.intent.name === 'RetirementReadinessIntent'
+            && request.dialogState === 'COMPLETED';
+    },
+    handle(handlerInput) {
+        const filledSlots = handlerInput.requestEnvelope.request.intent.slots;
 
-    const slotValues = getSlotValues(filledSlots);
+        const slotValues = getSlotValues(filledSlots);
 
-    const gap = getGap(slotvalues.currentBalance.resolved, slotvalues.investmentProfile.resolved, slotvalue.age.resolved, slotvalue.savingsPerYear.resolved, slotvalue.desiredIncome.resolved);
+        const gap = getGap(slotValues.currentBalance.resolved, slotValues.investmentProfile.resolved, slotValues.dateOfBirth.resolved, slotValues.savingsPerYear.resolved, slotValues.desiredIncome.resolved);
 
-    const speechOutput = `Your gap is ${gap} dollars`; // account for surplus senario
+        const speechOutput = (gap) => {
+            switch (gap) {
+                case gap < 0.9: {
+                    return `You have a deficit of ${gap} dollars. Consider contributing more to your retirement account.`;
+                }
+                case gap === 0.0: {
+                    return `Congratulations! You are on track.`;
+                }
+                case gap > 0.0: {
+                    return `Congratulations! You have a surplus of ${gap}.`;
+                }
+            }
+        };
 
-    return handlerInput.responseBuilder
-      .speak(speechOutput)
-      .getResponse();
-  },
+        return handlerInput.responseBuilder
+            .speak(speechOutput(gap))
+            .getResponse();
+    },
 };
 
 const HelpIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
-  },
-  handle(handlerInput) {
-    const speechText = 'You can say hello to me!';
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speechText = 'Let\'s calculate your retirement readiness.';
 
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt(speechText)
-      .withSimpleCard('Hello World', speechText)
-      .getResponse();
-  },
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Retirement Readiness', speechText)
+            .getResponse();
+    },
 };
 
 const CancelAndStopIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
-        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
-  },
-  handle(handlerInput) {
-    const speechText = 'Goodbye!';
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speechText = 'Goodbye!';
 
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('Hello World', speechText)
-      .getResponse();
-  },
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('Hello World', speechText)
+            .getResponse();
+    },
 };
 
 const SessionEndedRequestHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
-  },
-  handle(handlerInput) {
-    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
 
-    return handlerInput.responseBuilder.getResponse();
-  },
+        return handlerInput.responseBuilder.getResponse();
+    },
 };
 
 const ErrorHandler = {
-  canHandle() {
-    return true;
-  },
-  handle(handlerInput, error) {
-    console.log(`Error handled: ${error.message}`);
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
 
-    return handlerInput.responseBuilder
-      .speak('Sorry, I can\'t understand the command. Please say again.')
-      .reprompt('Sorry, I can\'t understand the command. Please say again.')
-      .getResponse();
-  },
+        return handlerInput.responseBuilder
+            .speak('Sorry, I can\'t understand the command. Please say again.')
+            .reprompt('Sorry, I can\'t understand the command. Please say again.')
+            .getResponse();
+    },
 };
 
 const skillBuilder = Alexa.SkillBuilders.custom();
 
+const requiredSlots = [
+    'dateOfBirth',
+    'currentBalance',
+    'savingsPerYear',
+    'desiredIncome',
+    'investmentProfile'
+];
+
 function getSlotValues(filledSlots) {
-  const slotValues = {};
+    const slotValues = {};
 
-  console.log(`The filled slots: ${JSON.stringify(filledSlots)}`);
-  Object.keys(filledSlots).forEach((item) => {
-    const name = filledSlots[item].name;
+    console.log(`The filled slots: ${JSON.stringify(filledSlots)}`);
+    Object.keys(filledSlots).forEach((item) => {
+        const name = filledSlots[item].name;
 
-    if (filledSlots[item] &&
-      filledSlots[item].resolutions &&
-      filledSlots[item].resolutions.resolutionsPerAuthority[0] &&
-      filledSlots[item].resolutions.resolutionsPerAuthority[0].status &&
-      filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code) {
-      switch (filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code) {
-        case 'ER_SUCCESS_MATCH':
-          slotValues[name] = {
-            synonym: filledSlots[item].value,
-            resolved: filledSlots[item].resolutions.resolutionsPerAuthority[0].values[0].value.name,
-            isValidated: true,
-          };
-          break;
-        case 'ER_SUCCESS_NO_MATCH':
-          slotValues[name] = {
-            synonym: filledSlots[item].value,
-            resolved: filledSlots[item].value,
-            isValidated: false,
-          };
-          break;
-        default:
-          break;
-      }
-    } else {
-      slotValues[name] = {
-        synonym: filledSlots[item].value,
-        resolved: filledSlots[item].value,
-        isValidated: false,
-      };
-    }
-  }, this);
+        if (filledSlots[item] &&
+            filledSlots[item].resolutions &&
+            filledSlots[item].resolutions.resolutionsPerAuthority[0] &&
+            filledSlots[item].resolutions.resolutionsPerAuthority[0].status &&
+            filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code) {
+            switch (filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code) {
+                case 'ER_SUCCESS_MATCH':
+                    slotValues[name] = {
+                        synonym: filledSlots[item].value,
+                        resolved: filledSlots[item].resolutions.resolutionsPerAuthority[0].values[0].value.name,
+                        isValidated: true,
+                    };
+                    break;
+                case 'ER_SUCCESS_NO_MATCH':
+                    slotValues[name] = {
+                        synonym: filledSlots[item].value,
+                        resolved: filledSlots[item].value,
+                        isValidated: false,
+                    };
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            slotValues[name] = {
+                synonym: filledSlots[item].value,
+                resolved: filledSlots[item].value,
+                isValidated: false,
+            };
+        }
+    }, this);
 
-  return slotValues;
+    return slotValues;
 }
 
-function getGap(currentBalance, investmentProfile, age, savingsPerYear, desiredIncome) {
-    
-    var fv, rate, periods, gap
+const getGap = (dateOfBirth, currentBalance, savingsPerYear, desiredIncome, investmentProfile) => {
 
-    // compound annually
-    periods = 65 - age
+    const periods = 65 - dateOfBirth;
 
-    switch (investmentProfile) {
-        case "aggressive":
-            rate = .07
-        case "balanced":
-            rate = .05
-        case "conservative":
-            rate = .04
-        default: 
-            rate = .04
-    }
+    const profiles = {
+        "aggressive": .07,
+        "balanced": .05,
+        "conservative": .04
+    };
 
-    fv = (currentBalance * ((1 + rate)**periods)) + (savingsPerYear*((((1+rate)*periods)-1)/rate))
+    const rate = profiles[investmentProfile];
 
-    gap = desiredIncome - fv
+    const futureValue = (currentBalance * (((1 + rate)*.97) ** periods)) + (savingsPerYear * (((((1 + rate) * .97) * periods) - 1) / rate));
 
-    return Math.round(gap*100)/100
-}
+    // Simple drawdown of 3%, balance doesn't change.
+    const projectedIncome = (futureValue * .03) / 12;
+
+    // Desired income adjusted for 3% inflation.
+    const adjustedDesiredIncome = desiredIncome * (1.03 ** periods);
+
+    // Desired - actual. Surplus if negative.
+    const gap = adjustedDesiredIncome - projectedIncome;
+
+    return Math.round(gap * 100) / 100;
+};
 
 exports.handler = skillBuilder
-  .addRequestHandlers(
-    LaunchRequestHandler,
-    InProgressIntent,
-    CompletedIntent,
-    HelpIntentHandler,
-    CancelAndStopIntentHandler,
-    SessionEndedRequestHandler
-  )
-  .addErrorHandlers(ErrorHandler)
-  .lambda();
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        InProgressIntent,
+        CompletedIntent,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler
+    )
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
